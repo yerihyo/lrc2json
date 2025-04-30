5c323b2a3047bee99f2c5616c35153a823a7231f
Moonyoung Kang <yerihyo@gmail.com>
2025-04-30 11:24:48 -0700

    updated

--- lrc2json/__main__.py
+++ lrc2json/__main__.py
@@ -7,21 +7,26 @@ def main():
     parser = argparse.ArgumentParser(
                     prog='lrc2json',
                     description='Convert LRC format into json with timestamps',
-                    usage="lrc2json: <lrc_file> <json_file>")
+                    usage="lrc2json: <lrc_file> <json_file> <srt_file>")
     
     parser.add_argument("lrc_file")
     parser.add_argument("json_file")
+    parser.add_argument("srt_file")
     args = parser.parse_args()
 
-    in_f   = open(args.lrc_file, "r", encoding="UTF-8")
-    lrc    = LRC(in_f)
-    lyrics = lrc.get_synced_lyrics()
+    with open(args.lrc_file, "r", encoding="UTF-8") as infile:
+        lines = [s.rstrip('\r\n') for s in infile if s]
 
-    out_f = open(args.json_file, "w", encoding="UTF-8")
-    out_f.write(json.dumps(lyrics))
 
-    in_f.close()
-    out_f.close()
+    infos = LRC.lines2infos(lines)
+    # infos = [info for str_line in lines for info in LRC.line2infos(str_line)]
+    hdocs = lmap(LRC.info2hdoc, infos)
+    with open(args.json_file, "w", encoding="UTF-8") as ofile:
+        ofile.write(json.dumps(hdocs, ensure_ascii=False))
+
+    srtlines = LRC.infos2srtlines(infos)
+    with open(args.srt_file, "w", encoding="UTF-8") as ofile:
+        print("\n\n".join(srtlines), file=ofile)
 
 if __name__ == "__main__":
     main()
--- lrc2json/lrc_parser.py
+++ lrc2json/lrc_parser.py
@@ -1,5 +1,11 @@
+import operator
 import re
-from typing import TextIO
+from functools import reduce
+from itertools import chain
+from typing import TextIO, Iterable
+
+from future.utils import lmap
+
 
 class LRC:
 
@@ -17,46 +23,89 @@ class LRC:
             line = '[{:02d}:{:02d}.{:02d}]{}'.format(minutes, seconds, remainder, lyrics[millis])
             if line[-1] != '\n': line += '\n'
             f.write(line)
-    
-    def get_synced_lyrics(self) -> dict:
 
-        lines = self.file.readlines()
-        synced_lyrics_dict = {}
+    @classmethod
+    def timestamp_str2msoffset(cls, s):
+        has_millis = '.' in s
+        pattern = '[\.:]' if has_millis else ':'
+        parts = re.split(pattern, s)
 
-        for lrc_line in lines:
-            
-            if len(lrc_line) == 0:
-                continue
-            
-            results = re.finditer('(\[\d\d\:\d\d\.\d+?(?=\])\]+)|(\[\d\d\:\d+?(?=\])\]+)', lrc_line)
+        minutes = int(parts[0])
+        seconds = int(parts[1])
+        millis = int(parts[2]) if has_millis else 0
+
+        return minutes * 60 * 1000 + seconds * 1000 + millis
+
+    @classmethod
+    def dicts2merged(cls, dicts:Iterable[dict]):
+        return reduce(lambda a, b: {**a, **b}, dicts)
+
+
+
+    @classmethod
+    def lines2infos(cls, lines: Iterable[str]):
+        def line2infos(line: str) -> Iterable[dict]:
+
+            # lines = file.readlines()
+            if not line:
+                return None
+
+            results = re.finditer('(\[\d\d\:\d\d\.\d+?(?=\])\]+)|(\[\d\d\:\d+?(?=\])\]+)', line)
             matches = [t for t in results]
 
             if len(matches) == 0:
+                return None
+
+            end_index = max([t.end(0) for t in matches])
+            lyric = line[end_index:].strip()
+            # if not lyrics:
+            #     return None
+
+            s_timestamps = [t.group(0)[1:-1] for t in matches]
+
+            for x in s_timestamps:
+                yield {
+                    "msoffset": cls.timestamp_str2msoffset(x),
+                    "lyric": lyric
+                }
+
+        infos = sorted(
+            chain.from_iterable(map(line2infos, lines)),
+            key=lambda x: x['msoffset']
+        )
+        return infos
+
+    @classmethod
+    def msoffset2str_srt(cls, msoffset):
+        hours = int(msoffset) // (1000*60*60)
+        minutes = (int(msoffset) % (1000*60*60)) // (1000*60)
+        seconds = (int(msoffset) % (1000*60)) // 1000
+        millisecs = msoffset % 1000
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d},{millisecs:03d}"
+
+    @classmethod
+    def infos2srtlines(cls, infos:Iterable[dict]):
+        # strs_msoffset = lmap(lambda info_: cls.msoffset2str_srt(info_['msoffset']), infos)
+
+        for i, info in enumerate(infos):
+            if not info['lyric']:
                 continue
 
-            end_index    = max([t.end(0) for t in matches])
-            lyrics       = lrc_line[end_index:]
-            s_timestamps = [t.group(0) for t in matches]
-            
-            for s_timestamp in s_timestamps:
+            msoffset_start = infos[i]['msoffset']
+            msoffset_end = infos[i+1]['msoffset'] if i+1 < len(infos) else msoffset_start + 1000
+            yield "\n".join([
+                str(i+1),
+                f"{cls.msoffset2str_srt(msoffset_start)} --> {cls.msoffset2str_srt(msoffset_end)}",
+                info['lyric']
+            ])
 
-                s_timestamp = s_timestamp[1:-1]
-                
-                minutes = 0
-                seconds = 0
-                millis  = 0
-
-                has_millis = '.' in s_timestamp
-                pattern    = '[\.:]' if has_millis else ':'
-                parts      = re.split(pattern, s_timestamp)
-
-                minutes = int(parts[0])
-                seconds = int(parts[1])
-
-                if has_millis:
-                    millis = int(parts[2])
-
-                timestamp = minutes * 60 * 1000 + seconds * 1000 + millis
-                synced_lyrics_dict[timestamp] = lyrics
-
-        return synced_lyrics_dict
+    @classmethod
+    def info2hdoc(cls, info: dict) -> dict:
+        return {info['msoffset']: info['lyric']}
+    # @classmethod
+    # def lines_lrc2dict(cls, lines: Iterable[str]) -> dict:
+    #     return {
+    #         info['msoffset']: info['lyric']
+    #         for line in lines
+    #         for info in cls.line2infos(line)
+    #     }
